b40254bd473824a136aa643ca9e8e4c733f4ae95
DarKreter <darkreter@gmail.com>
2023-10-14 13:43:39 +0200

    init

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, int** argv)
+{
+    printf("Hello World!\n");
+
+    return 0;
+}
+
+// TODO: 
+// Read /proc/stat
+// Calculate processor usage
+// Print it
+// Create 3 threads with pthread
+// Connect reader and analyzer
+// Connect analyzer with printer
